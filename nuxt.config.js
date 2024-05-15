af704e9e29aaf4766d6b8a82676222553b9fae18
Jay <j19950310@gmail.com>
2024-05-15 15:02:30 +0800

    CD no ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@
 
 const isProduction = process.env.NODE_ENV === "production";
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   modules: ["@vueuse/nuxt"],
   app: {

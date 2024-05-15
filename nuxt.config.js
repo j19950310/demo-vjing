5777a22205d87200ae823b83c65acb1a7b50f1b0
Jay <j19950310@gmail.com>
2024-05-15 14:55:37 +0800

    fix nuxt baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const isProduction = process.env.NODE_ENV === "production";
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@vueuse/nuxt"],
+  app: {
+    baseURL: isProduction ? "https://j19950310.github.io/demo-vjing/" : "/",
+  }
 });
